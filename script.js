28c428851cd1275811f8ac4f2c924b117f9eaba6
SurjeetMandal <surjeetmandal698@gmail.com>
2024-05-10 12:40:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    let age = document.getElementById('age').value;
+    let name = document.getElementById('name').value;
+
+    if (!age || !name) {
+        alert('Both age and name are required!');
+        return;
+    }
+
+function checkAge(age, name) {
+    return new Promise((resolve, reject) => {
+        if (age >= 18) {
+            setTimeout(() => {
+                resolve("Welcome, " + name + ". You can vote.");
+            }, 4000);
+        } else {
+            reject("Oh sorry " + name + ". You aren't old enough.");
+        }
+    });
+}
+    checkAge(age, name).then(data1 => alert(data1)).catch(data2 => alert(data2));
+});
